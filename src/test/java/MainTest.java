dc8f4a84949a5a7b72d0f3eb43f2af8bd59bb899
Viktoria.Bozhko <vbo.secondary@gmail.com>
2023-02-23 17:39:46 +0100

    Change

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -14,7 +14,7 @@ public class MainTest {
 
     @Test
     public void testIfFalse() {
-        Assertions.assertTrue(false, "This is FALSE, MF!");
+        Assertions.assertTrue(false, "This is FAAAAAAAAALSE!!!!!!");
     }
 
 }
