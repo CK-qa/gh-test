95dcab16ef2dd46d01e59e87815de1bdea3846ab
Viktoria.Bozhko <vbo.secondary@gmail.com>
2023-03-02 16:56:21 +0100

    commit

--- src/main/java/org/example/NewClass.java
+++ src/main/java/org/example/NewClass.java
@@ -4,7 +4,7 @@ public class NewClass {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("ONE more");
+            System.out.println("ONEmore");
         }
     }
 }
